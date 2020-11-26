915833436d1b96b39d57c5c4b5a2b73abaa8cdee
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2020-11-26 19:26:44 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-function szöbeg_kiírása_2 (szöveg: string, meddig: number, szünet: number) {
+input.onButtonPressed(Button.A, function () {
+    szöveg_kiírása_2("Hello!", 500, 200)
+})
+function szöveg_kiírása_2 (szöveg: string, meddig: number, szünet: number) {
     for (let index = 0; index <= szöveg.length; index++) {
         basic.showString(szöveg.substr(index, 1))
         basic.pause(meddig)
@@ -6,9 +9,6 @@ function szöbeg_kiírása_2 (szöveg: string, meddig: number, szünet: number) 
         basic.pause(szünet)
     }
 }
-input.onButtonPressed(Button.A, function () {
-    szöbeg_kiírása_2("Hello!", 500, 200)
-})
 basic.forever(function () {
 	
 })
